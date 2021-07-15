d901891d4fcf604929fc852519a01d717e09ac78
JEMMA GILBERT <jg218537@falmouth.ac.uk>
2021-07-15 21:19:36 +0100

    Added:
    - Dialog system (SO + Controller)
    - Some dialog SOs;
    
    Edited:
    
    - Main scene with these test lines.
    
    Signed-off-by: JEMMA GILBERT

---
+++ GJL_2021_NameTBD/Assets/Scripts/DialogSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialog", menuName = "Dialog", order = 1)]
+public class DialogSO : ScriptableObject
+{
+    public string[] dialog;
+}
